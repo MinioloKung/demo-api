6e17d8bae4eccc695b6bb64024654a0ed984ae8f
MinioloKung <jeffyfilm2kp@gmail.com>
2026-05-13 23:45:07 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+// import required modules
+const express = require('express');  // Framework for creating web server
+const bodyParser = require('body-parser');  // JSON data parser from request body
+const cors = require('cors');   // Allow access from other domains
+
+// create Express application
+const app = express();
+const port = 5000;  // Define the port where server will run
+
+// install middleware
+app.use(cors());  // Enable CORS for all routes
+app.use(bodyParser.json());  // Parse JSON in request body to JavaScript object
+
+// main route - test if server is working
+app.get('/', (req, res) => {
+  res.status(200).send('Hello! RESTful API is ready');
+});
+
+// Start sever and listen for requests
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
